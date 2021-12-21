0ca6c2d24dfc4de02a4100248f9d1a6c4fc3097d
SebastianBoettger <60438900+SebastianBoettger@users.noreply.github.com>
2021-12-21 17:22:49 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_HalloJan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hallo Jan, mein Name ist Sebastian!");
+            Console.ReadKey();
+        }
+    }
+}
